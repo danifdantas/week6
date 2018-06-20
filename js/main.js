b5f9e1fcd8b79d0ea565f2138d2f9b669bf40caa
Dani Dantas <dani@DaniVonRohr.fc.ca>
2018-06-20 14:35:16 -0400

    added subtract to calculator

--- js/main.js
+++ js/main.js
@@ -10,6 +10,7 @@ var equals = document.querySelector("#trigger");
 var outcome = document.querySelector("#value");
 var selPlus = document.querySelector("#plus");
 var selMinus = document.querySelector("#minus");
+var userChoice = ""; //global variable. it is not assigned to a function
 // console.log(num1);
 // console.log(num2);
 // console.log(equals);
@@ -19,7 +20,9 @@ var selMinus = document.querySelector("#minus");
 
 
 //Functions
-function addStuff(){
+function doStuff(){
+	var total = "";
+	console.log(userChoice);
 	// console.log("from addStuff");
 	// document.getElementById("input1").value;
 	// document.getElementById("input2").value;
@@ -28,25 +31,50 @@ function addStuff(){
 	// console.log (num1.value);
 	// console.log (num2.value);
 	/* parseInt converts the value / string to a number */
-	var total = parseInt(num1.value) + parseInt(num2.value);
+	if(userChoice == "add"){
+	 total = parseInt(num1.value) + parseInt(num2.value);
+	} else {
+		total = parseInt(num1.value) - parseInt(num2.value);
+	}
 	// console.log(total);
 	outcome.innerHTML = total;
 	/* innerHTML will go inside the id = value where the outcome is connected to
 	inside HTML and show it there */
 }
 
-function subtractStuff(){
-	var total = parseInt(num1.value) - parseInt(num2.value);
+function selCal(evt){
+	// console.log("From selCal");
+	/*if(selPlus.innerHTML == "+") {
+		console.log("User clicked +");
+	}
+	if(selPlus.innerHTML == "-"){
+		console.log("User clicked -");
+	}*/
+	// console.log(evt.currentTarget.id);
+	if(evt.currentTarget.id == "plus"){
+		// console.log("add");
+		userChoice = "add";
 
-	outcome.innerHTML = total;
+	}else{
+		// console.log("sub");
+		userChoice = "sub";
+	}
 }
+// function subtractStuff(){
+// 	var total = parseInt(num1.value) - parseInt(num2.value);
+
+// 	outcome.innerHTML = total;
+// }
 
 
 //Listeners
 
-selPlus.addEventListener("click", addStuff, false);
-selMinus.addEventListener("click", subtractStuff, false);
-// equals.addEventListener("click", addStuff, false);
+
+// selPlus.addEventListener("click", addStuff, false);
+// selMinus.addEventListener("click", subtractStuff, false);
+equals.addEventListener("click", doStuff, false);
+selPlus.addEventListener("click", selCal, false);
+selMinus.addEventListener("click", selCal, false);
 
 
 
